0856c6a9d594856ce9978f9554ef16b9294f0f03
Oleksandr Merlenko <s.merlenko@gmail.com>
2024-10-21 17:22:25 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,21 @@
 from app.main import get_coin_combination
 
 
-# write your tests here
+def test_return_zeroes_if_cents_equals_zero() -> None:
+    assert get_coin_combination(0) == [0, 0, 0, 0]
+
+
+def test_return_1_penny_if_cents_equals_1() -> None:
+    assert get_coin_combination(1) == [1, 0, 0, 0]
+
+
+def test_return_a_nickel_if_cents_equals_5_and_more() -> None:
+    assert get_coin_combination(6) == [1, 1, 0, 0]
+
+
+def test_return_a_dime_if_cents_equals_15_and_more() -> None:
+    assert get_coin_combination(17) == [2, 1, 1, 0]
+
+
+def test_return_a_quarter_if_cents_equals_25_and_more() -> None:
+    assert get_coin_combination(26) == [1, 0, 0, 1]
